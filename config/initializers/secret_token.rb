878259f32d02636118cd0a720d1e9292b609b2a4
Kalló Bernát <kallo.bernat@gmail.com>
2011-06-06 01:06:32 +0200

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chiller::Application.config.secret_token = 'c63ee5f8c3930a371e23d35a1cb6f5705145f60eebd67ef90246be6439bb8a4ddba9225c7b6e864c474ed06a27bee27606a927fcf91cb337e4858fda01a562db'
